3e68c17f8dd4246d55008cb414076ab52769175f
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-24 20:55:17 +0300

    Create Categories

--- getir-clone/src/components/Categories.js
+++ getir-clone/src/components/Categories.js
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react"
+import categoryData from '../api/categories.json'
+import Category from './ui/Category'
+
 function Categories() {
+
+const [categories, setCategories]=useState([]);
+
+useEffect(() => {
+  // istek atıyoruz
+  // setTimeout(() => setCategories(categoryData), 1000)
+  setCategories(categoryData)
+}, [])
+
     return (
-      <div>
-        Categories
+      <div className="bg-white py-4">
+        <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
+        <div className="grid grid-cols-10">
+        {/* {!categories.length && 'Yükleniyor..'} */}
+        {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+        </div>
       </div>
     )
   }
