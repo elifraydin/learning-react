79281911bff95c727ff54c48fbf04d02197b944f
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-24 22:18:45 +0300

    Create Campaigns component

--- getir-clone/src/components/Campaigns.js
+++ getir-clone/src/components/Campaigns.js
@@ -1,8 +1,68 @@
+import Slider from "react-slick";
+import { useState, useEffect } from "react";
+import Banners from '../api/banners.json'
+import Title from "./ui/Title";
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+
+function NextBtn({ className, style, onClick }) {
+  return (
+      <button className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`} onClick={onClick}>
+          <IoIosArrowForward size={22} />
+     </button>
+  )
+}
+
+function PrevBtn({ className, style, onClick }) {
+  return (
+      <button className={`text-brand-color absolute top-1/2 -left-6 -translate-y-1/2`} onClick={onClick}>
+          <IoIosArrowBack size={22} />
+      </button>
+  )
+}
+
+
 function Campaigns() {
-    return (
-      <div>
-        Campaigns
-      </div>
-    )
-  }
+
+  const [banners, setBanners] = useState([]);
+
+  useEffect(() => {
+    setBanners(Banners);
+  }, [])
+
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,//sayfada 3 tane görünür
+    slidesToScroll: 1, //birer birer kayar
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    cssEase: "linear",
+    nextArrow: <NextBtn />,
+		prevArrow: <PrevBtn />,
+
+  };
+
+
+  return (
+    <div className="container mx-auto py-8">
+      <Title>Kampanyalar</Title>
+      <Slider className="-mx-2" {...settings}>
+        {banners.length && banners.map((banner) => (
+          <div key={banner.id}>
+            <picture className=" block px-2">
+              <img
+                alt="banner"
+                className="w-full object-cover rounded-lg"
+                src={banner.image}
+              />
+            </picture>
+          </div>
+        ))}
+
+      </Slider>
+    </div>
+  )
+}
 export default Campaigns
--- getir-clone/src/components/Categories.js
+++ getir-clone/src/components/Categories.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import categoryData from '../api/categories.json'
 import Category from './ui/Category'
+import Title from "./ui/Title";
 
 function Categories() {
 
@@ -14,10 +15,12 @@ useEffect(() => {
 
     return (
       <div className="bg-white py-4">
-        <h3 className="text-sm font-semibold mb-3">Kategoriler</h3>
-        <div className="grid grid-cols-10">
+        <div className="container mx-auto">
+        <Title>Kategoriler</Title>
+         <div className="grid grid-cols-10">
         {/* {!categories.length && 'Yükleniyor..'} */}
         {categories && categories.map((category, index) => <Category key={index} category={category} />)}
+        </div>
         </div>
       </div>
     )
