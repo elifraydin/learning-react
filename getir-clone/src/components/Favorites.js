e338753caedb4bd7d378243266470b8d5ac35709
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-24 22:41:31 +0300

    Create Favorites component

--- getir-clone/src/components/Favorites.js
+++ getir-clone/src/components/Favorites.js
@@ -1,8 +1,24 @@
+import { useState, useEffect } from "react";
+import Title from "./ui/Title";
+import Products from '../api/products.json'
+import ProductItem from './ui/ProductItem'
+
 function Favorites() {
+
+  const [products, setProducts] = useState([]);
+
+ useEffect(() => {
+        setProducts(Products);
+    }, [])
+
+
     return (
       <div>
-        Favorites
-      </div>
+            <Title>Favoriler</Title>
+            <div className="grid grid-cols-8 gap-0.1 rounded-lg overflow-hidden">
+                {products.length && products.map((product) => <ProductItem key={product.id} product={product} />)}
+            </div>
+        </div>
     )
   }
 export default Favorites
