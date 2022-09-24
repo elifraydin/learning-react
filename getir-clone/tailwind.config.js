569d0b6a5a4acd32b641643c9e4da2ca9e6f14fb
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-24 17:12:14 +0300

    Create Header component

--- getir-clone/tailwind.config.js
+++ getir-clone/tailwind.config.js
@@ -4,7 +4,14 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+
+      colors: {
+        'brand-color': '#5d3ebc',
+      },
+
+
+    },
   },
   plugins: [],
 }
