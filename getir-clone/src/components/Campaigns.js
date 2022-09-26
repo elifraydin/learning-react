88a0b08f3c777d138372b7f4ef8060f86b71d806
elif rumeysa aydin <elifrumeysaaydin@gmail.com>
2022-09-26 16:05:34 +0300

    Header, Campaigns Responsive Design

--- getir-clone/src/components/Campaigns.js
+++ getir-clone/src/components/Campaigns.js
@@ -4,6 +4,7 @@ import Banners from '../api/banners.json'
 import Title from "./ui/Title";
 import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
 
+
 function NextBtn({ className, style, onClick }) {
   return (
     <button className={`text-brand-color absolute top-1/2 -right-6 -translate-y-1/2`} onClick={onClick}>
@@ -44,41 +45,43 @@ function Campaigns() {
 
     responsive: [
       {
-          breakpoint: 1280,
-          settings: {
-              slidesToShow: 3,
-              arrows: false,
-          }
+        breakpoint: 1280,
+        settings: {
+          slidesToShow: 3,
+          arrows: false,
+        }
       },
       {
-          breakpoint: 1024,
-          settings: {
-              slidesToShow: 2,
-              arrows: false,
-          }
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          arrows: false,
+        }
       },
       {
-          breakpoint: 768,
-          settings: {
-              slidesToShow: 1,
-              arrows: false,
-          }
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          arrows: false,
+        }
       }
-  ]
+    ]
 
   };
 
 
   return (
-    <div className="container mx-auto py-8">
-      <Title>Kampanyalar</Title>
-      <Slider className="-mx-2" {...settings}>
+    <div className="container mx-auto md:pt-8">
+      <div className="hidden md:block">
+        <Title>Kampanyalar</Title>
+      </div>
+      <Slider className="md:-mx-2" {...settings}>
         {banners.length && banners.map((banner) => (
           <div key={banner.id}>
-            <picture className=" block px-2">
+            <picture className=" block md:px-2">
               <img
                 alt="banner"
-                className="w-full object-cover rounded-lg"
+                className="w-full object-cover md:rounded-lg"
                 src={banner.image}
               />
             </picture>
